82a6182f774d31e2b2b83d6af61da77889b93f22
Michael CARBENAY <michael@carbenay.info>
2022-10-29 14:28:18 +0200

    Création function vide smartthings

---
+++ Home.Azure.Functions/SmartThings.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Home.Azure.Functions
+{
+    public static class SmartThings
+    {
+        [FunctionName("SmartThings")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            return new OkObjectResult(JsonConvert.SerializeObject(data));
+        }
+    }
+}
